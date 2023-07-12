a5f6041c18eb9da90f51840da24e330827964317
David Sanchez <davidsanchezr81@gmail.com>
2023-07-12 23:58:37 +0100

    first graphql API

---
+++ schema/FakeData.js
@@ -0,0 +1,25 @@
+const UserList = [
+{
+    id:1,
+    name: "John",
+    username:"john wick",
+    age:20,
+    nationality:"Canada"
+},
+{
+    id:2,
+    name: "Maria",
+    username:"maria la del barrio",
+    age:14,
+    nationality:"Venezuela"
+},
+{
+    id:3,
+    name: "Pedro",
+    username:"Pedro Navaja",
+    age:23,
+    nationality:"Colombia"
+},
+] 
+
+module.exports = { UserList };
