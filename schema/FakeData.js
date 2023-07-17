8c790b89f2f85b4798f801ec9bfd619adcd3c4cd
David Sanchez <davidsanchezr81@gmail.com>
2023-07-17 13:05:40 +0100

    more data types

--- schema/FakeData.js
+++ schema/FakeData.js
@@ -4,7 +4,23 @@ const UserList = [
     name: "John",
     username:"john wick",
     age:20,
-    nationality:"Canada"
+    nationality:"Canada",
+    friends: [
+        {
+            id:2,
+            name: "Maria",
+            username:"maria la del barrio",
+            age:14,
+            nationality:"Venezuela"
+        },
+        {
+            id:3,
+            name: "Pedro",
+            username:"Pedro Navaja",
+            age:23,
+            nationality:"Colombia"
+        },
+    ]
 },
 {
     id:2,
@@ -20,6 +36,30 @@ const UserList = [
     age:23,
     nationality:"Colombia"
 },
-] 
+];
 
-module.exports = { UserList };
+const MovieList = [
+    {
+        id: 1,
+        name: "Avengers",
+        yearOfPublication: 2019,
+        isInTheaters: true
+        
+    },
+    {
+        id: 2,
+        name: "Avengers II",
+        yearOfPublication: 2020,
+        isInTheaters: false
+        
+    },
+    {
+        id: 3,
+        name: "Avengers III",
+        yearOfPublication: 2021,
+        isInTheaters: true
+        
+    },
+    ] 
+
+module.exports = { UserList, MovieList };
